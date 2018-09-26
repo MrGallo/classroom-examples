82921cb5f322aab7959af8c1c96bdc872a5116b6
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-09-26 08:51:57 -0400

    Create textbook_tuesday.py

---
+++ textbook_tuesday.py
@@ -0,0 +1,9 @@
+# Inspired by Chapter 4
+# Start off rock, scissor, paper with a very simple implementation of choice checking.
+
+user_choice = input("Choose rock scissor or paper:")
+
+if user_choice == 'rock':
+    result = "win"
+else:
+    result = "lose"
