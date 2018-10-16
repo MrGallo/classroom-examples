00fae69638dc61928a301d9ea34d11413a2613bb
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-10-16 09:52:43 -0400

    Create intro-to-processing.py

---
+++ processing/intro-to-processing.py
@@ -0,0 +1,13 @@
+size(640, 480)
+background(255, 255, 255)  # colors are in RGB
+
+# ellipse(x, y, width, height)
+noStroke()
+fill(255, 0, 120)
+ellipse(width/2, height/2, 100, 100)
+
+# rect(x, y, width, height)
+stroke(0)
+strokeWeight(10)
+fill(0, 180, 0)
+rect(width/2, height/2, 200, 200)
