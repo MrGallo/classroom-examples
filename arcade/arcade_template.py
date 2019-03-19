17585cbcee4f202facdb5a85d377297bac9397a3
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-03-19 08:59:07 -0400

    add arcade_template.py

---
+++ arcade/arcade_template.py
@@ -0,0 +1,43 @@
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+
+
+def update(delta_time):
+    pass
+
+
+def on_draw():
+    arcade.start_render()
+
+
+def on_key_press(key, modifiers):
+    pass
+
+
+def on_key_release(key, modifiers):
+    pass
+
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+def setup():
+    arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1/60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+
+    arcade.run()
+
+
+if __name__ == '__main__':
+    setup()
