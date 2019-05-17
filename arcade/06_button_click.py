d01bcc3e78c3e526e1e539fe6f15e54de3229033
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-05-17 11:18:56 -0400

    Change button example

--- arcade/06_button_click.py
+++ arcade/06_button_click.py
@@ -14,8 +14,21 @@ import arcade
 WIDTH = 640
 HEIGHT = 480
 
-# There are better ways to represent buttons
-my_button = [100, 200, 150, 50]  # x, y, width, height
+# Key for button index values
+# This makes dealing with buttons as lists slightly easier.
+BTN_X = 0
+BTN_Y = 1
+BTN_WIDTH = 2
+BTN_HEIGHT = 3
+BTN_IS_CLICKED = 4
+BTN_COLOR = 5
+BTN_CLICKED_COLOR = 6
+
+# You might want to Google:
+# - python namedtuple
+# - python classes
+# Those would be better ways to store objects like our button.
+button1 = [200, 200, 300, 50, False, arcade.color.BLUE, arcade.color.GREEN]
 
 
 def setup():
@@ -29,6 +42,7 @@ def setup():
     window.on_key_press = on_key_press
     window.on_key_release = on_key_release
     window.on_mouse_press = on_mouse_press
+    window.on_mouse_release = on_mouse_release
 
     arcade.run()
 
@@ -40,11 +54,19 @@ def update(delta_time):
 def on_draw():
     arcade.start_render()
     # Draw in here...
-    arcade.draw_xywh_rectangle_filled(my_button[0],
-                                      my_button[1],
-                                      my_button[2],
-                                      my_button[3],
-                                      arcade.color.BLACK)
+
+    # Select the appropriate color to draw with
+    if button1[BTN_IS_CLICKED]:
+        color = button1[BTN_CLICKED_COLOR]
+    else:
+        color = button1[BTN_COLOR]
+
+    # Draw button1
+    arcade.draw_xywh_rectangle_filled(button1[BTN_X],
+                                      button1[BTN_Y],
+                                      button1[BTN_WIDTH],
+                                      button1[BTN_HEIGHT],
+                                      color)
 
 
 def on_key_press(key, modifiers):
@@ -56,15 +78,17 @@ def on_key_release(key, modifiers):
 
 
 def on_mouse_press(x, y, button, modifiers):
-    # unpack the button list into readable? variables.
-    my_button_x, my_button_y, my_button_w, my_button_h = my_button
+    print(f"Click at ({x}, {y})")
 
-    # Need to check all four limits of the button.
-    if (x > my_button_x and x < my_button_x + my_button_w and
-            y > my_button_y and y < my_button_y + my_button_h):
-        print("Clicked!!!")
-    else:
-        print("not clicked")
+    # Check if click happened on button1 
+    if (x > button1[BTN_X] and x < button1[BTN_X] + button1[BTN_WIDTH] and
+            y > button1[BTN_Y] and y < button1[BTN_Y] + button1[BTN_HEIGHT]):
+        button1[BTN_IS_CLICKED] = True
+
+
+def on_mouse_release(x, y, button, modifiers):
+    # When you let go of the mouse, all buttons should be set to False.
+    button1[BTN_IS_CLICKED] = False
 
 
 if __name__ == '__main__':
