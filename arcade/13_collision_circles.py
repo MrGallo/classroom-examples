6f422d1e7341e376a4b54e1c6d5140fdfb3539de
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-05-27 10:19:32 -0400

    Create collision circles

---
+++ arcade/13_collision_circles.py
@@ -0,0 +1,58 @@
+"""
+Determine if two circles are close enough that
+they are considered 'touching'.
+
+Basically:
+- Two circles have an (x,y) location and a radius.
+- Find distance between two points.
+- If the distance is less than the two radii, they are
+  'touching'.
+"""
+
+
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+
+
+def update(delta_time):
+    pass
+
+
+def on_draw():
+    arcade.start_render()
+    # Draw in here...
+    arcade.draw_circle_filled(100, 100, 25, arcade.color.BLUE)
+
+
+def on_key_press(key, modifiers):
+    pass
+
+
+def on_key_release(key, modifiers):
+    pass
+
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+def setup():
+    arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1/60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+    window.on_mouse_press = on_mouse_press
+
+    arcade.run()
+
+
+if __name__ == '__main__':
+    setup()
