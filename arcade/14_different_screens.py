3073eee2b1267615d9ac6cfc1497535ab50571aa
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-05-27 14:15:35 -0400

    Factor out draw functions

--- arcade/14_different_screens.py
+++ arcade/14_different_screens.py
@@ -30,25 +30,11 @@ def on_draw():
     arcade.start_render()
     # Draw in here...
     if current_screen == "menu":
-        arcade.set_background_color(arcade.color.WHITE_SMOKE)
-        arcade.draw_text("Main Menu", WIDTH/2, HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
-        arcade.draw_text("I for Instructions", WIDTH/2, HEIGHT/2-60,
-                         arcade.color.BLACK, font_size=20, anchor_x="center")
-        arcade.draw_text("P to Play", WIDTH/2, HEIGHT/2-90,
-                         arcade.color.BLACK, font_size=20, anchor_x="center")
+        draw_menu()
     elif current_screen == "instructions":
-        arcade.set_background_color(arcade.color.BLUE_GRAY)
-        arcade.draw_text("Instructions", WIDTH/2, HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
-        arcade.draw_text("ESC to go back", WIDTH/2, HEIGHT/2-60,
-                         arcade.color.BLACK, font_size=20, anchor_x="center")
+        draw_instructions()
     elif current_screen == "play":
-        arcade.set_background_color(arcade.color.ORANGE_RED)
-        arcade.draw_text("Play", WIDTH/2, HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
-
-        arcade.draw_circle_filled(ball_x, 100, 30, arcade.color.WHITE)
+        draw_play()
 
 
 def on_key_press(key, modifiers):
@@ -88,6 +74,32 @@ def setup():
     window.on_mouse_press = on_mouse_press
 
     arcade.run()
+
+
+def draw_menu():
+    arcade.set_background_color(arcade.color.WHITE_SMOKE)
+    arcade.draw_text("Main Menu", WIDTH/2, HEIGHT/2,
+                     arcade.color.BLACK, font_size=30, anchor_x="center")
+    arcade.draw_text("I for Instructions", WIDTH/2, HEIGHT/2-60,
+                     arcade.color.BLACK, font_size=20, anchor_x="center")
+    arcade.draw_text("P to Play", WIDTH/2, HEIGHT/2-90,
+                     arcade.color.BLACK, font_size=20, anchor_x="center")
+
+
+def draw_instructions():
+    arcade.set_background_color(arcade.color.BLUE_GRAY)
+    arcade.draw_text("Instructions", WIDTH/2, HEIGHT/2,
+                     arcade.color.BLACK, font_size=30, anchor_x="center")
+    arcade.draw_text("ESC to go back", WIDTH/2, HEIGHT/2-60,
+                     arcade.color.BLACK, font_size=20, anchor_x="center")
+
+
+def draw_play():
+    arcade.set_background_color(arcade.color.ORANGE_RED)
+    arcade.draw_text("Play", WIDTH/2, HEIGHT/2,
+                     arcade.color.BLACK, font_size=30, anchor_x="center")
+
+    arcade.draw_circle_filled(ball_x, 100, 30, arcade.color.WHITE)
 
 
 if __name__ == '__main__':
