11dd039aa57ea56e7325bba56551413a3284c54b
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-10-02 10:37:21 -0400

    Create 01-test-builtin-functions.py

---
+++ 09-testing/01-test-builtin-functions.py
@@ -0,0 +1,23 @@
+import math
+
+
+
+# test sum(numbers: List[float]) -> float
+def test_sum():
+    assert sum([]) == 0
+    assert sum([1, 2, 3]) == 6
+    assert sum([5, 5, 5]) == 15
+    assert sum([1] * 1000) == 1000
+
+
+# test math.ceil(float) -> int
+def test_math_ceil():
+    assert math.ceil(6.0001) == 7
+    assert math.ceil(6.0) == 6
+    assert math.ceil(6.9999) == 7
+
+
+# test math.floor(float) -> int
+# test f strings
+# test .format()
+
