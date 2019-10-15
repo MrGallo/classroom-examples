489f0440681a3c57ace29cfb187863220711f059
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-10-15 11:17:33 -0400

    Initial code

---
+++ 10-classes/02-class-exercise-solution.py
@@ -0,0 +1,10 @@
+class Person:
+    """
+    Attrs:
+        height (int): in cm
+        name (str): First and last
+        strength (int): How strong the person is 
+        health_points (int): Out of 100 (everyone starts at 100)
+    """
+    def __init__(self, ...):
+        pass
