35e6bfc273b5ce027b0c82f6c59a8c83bf7dd992
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-10-15 12:36:25 -0400

    Add __init__ method

--- 10-classes/02-class-exercise-solution.py
+++ 10-classes/02-class-exercise-solution.py
@@ -6,5 +6,11 @@ class Person:
         strength (int): How strong the person is 
         health_points (int): Out of 100 (everyone starts at 100)
     """
-    def __init__(self, ...):
-        pass
+    def __init__(self, name, height, strength):
+        self.name = name
+        self.height = height
+        self.strength = strength
+        self.health_points = 100
+
+
+jeff = Person("Jeff Blah", 170, 1)
