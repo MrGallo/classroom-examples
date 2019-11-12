3851b317589b9333007c6d511fcb73fba4d438c6
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-11-12 13:23:20 -0500

    Base code

---
+++ arcade/create_sprite.py
@@ -0,0 +1,43 @@
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+
+window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+
+
+def setup():
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1/60)
+    arcade.run()
+
+
+def update(delta_time):
+    pass
+
+
+@window.event
+def on_draw():
+    arcade.start_render()
+    # Draw in here...
+    arcade.draw_circle_filled(100, 100, 25, arcade.color.BLUE)
+
+
+@window.event
+def on_key_press(key, modifiers):
+    pass
+
+
+@window.event
+def on_key_release(key, modifiers):
+    pass
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+if __name__ == '__main__':
+    setup()
