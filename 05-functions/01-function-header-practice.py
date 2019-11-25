2348c3c2c23847131382c4fad3d589c1613cf4f1
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-11-25 09:25:16 -0500

    Create 01-function-header-practice.py

---
+++ 05-functions/01-function-header-practice.py
@@ -0,0 +1,18 @@
+# Write function headers and docstring for the following:
+
+# sum
+
+
+# math.ceil
+
+
+# math.floor
+
+
+# Function that takes a name and returns a message
+
+
+# Function that takes two floats and finds their average.
+
+
+# Function that takes an age and returns True if they are of voting age
