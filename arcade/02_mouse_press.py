d45d176218b62cc32ee0cef29ef827dd6e229818
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2020-02-18 12:52:24 -0500

    Base Template

---
+++ arcade/02_mouse_press.py
@@ -0,0 +1,22 @@
+import arcade
+
+WIDTH = 800
+HEIGHT = 600
+
+window = arcade.Window(WIDTH, HEIGHT, "My Arcade Game")
+arcade.set_background_color(arcade.color.AMAZON)
+
+# Initialize your variables here
+
+@window.event("on_draw")
+def game_loop():
+    # import global variables here.
+
+    # update your variables here.
+
+    # Draw things here.
+    arcade.start_render()
+    arcade.draw_circle_filled(100, 100, 25, arcade.color.CREAM)
+
+
+arcade.run()
