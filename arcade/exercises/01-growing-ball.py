16004495be6140c53ff68209f7ddc5b9b4bdf455
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2020-02-19 11:53:41 -0500

    Base template

--- arcade/exercises/01-growing-ball.py
+++ arcade/exercises/01-growing-ball.py
@@ -22,3 +22,26 @@ From here you can do many things:
   the score using arcade.draw_text().
 - You could also keep track of the accuracy of the clicks by tracking all clicks that missed the ball.
 """
+
+import arcade
+
+WIDTH = 800
+HEIGHT = 600
+
+window = arcade.Window(WIDTH, HEIGHT, "My Arcade Game")
+arcade.set_background_color(arcade.color.AMAZON)
+
+# Initialize your variables here
+
+@window.event("on_draw")
+def game_loop():
+    # import global variables here.
+
+    # update your variables here.
+
+    # Draw things here.
+    arcade.start_render()
+    arcade.draw_circle_filled(100, 100, 25, arcade.color.CREAM)
+
+
+arcade.run()
